54c3738c57b3cea84e378a899925f1957a36ffce
Harshith A K <harshithak5520@gmail.com>
2026-07-31 22:51:19 +0530

    Implement twoSum function in Two_sum.py

---
+++ 1-50/01. Two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for i in range(len(nums)):
+            result=target-nums[i]
+            if result in hashmap:
+                return [hashmap[result],i]
+            
+            hashmap[nums[i]]=i
